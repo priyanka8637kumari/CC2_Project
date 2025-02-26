4ee230785a10d0db94927f86d6b7f868208717b4
Priyanka <priyanka.kumari@hyperisland.se>
2025-02-26 15:34:54 +0100

    Update css variable and first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import LandingNavbar from './Components/LandingPage/LandingNavbar/LandingNavbar'
+
+
+import styles from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-     <h1>Welcome to Our Healthcare App</h1>
+     <div className={styles.App}>
+      <LandingNavbar />
+
+     </div>
     </>
   )
 }
